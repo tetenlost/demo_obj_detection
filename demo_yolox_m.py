41b3677ac7e4d9f14c4f2730208884feacd04baf
tetenlost <38835841+tetenlost@users.noreply.github.com>
2022-10-31 17:24:55 +0800

    Create demo_yolox_m.py

---
+++ demo_yolox_m.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# Copyright (c) Megvii, Inc. and its affiliates.
+import os
+
+from yolox.exp import Exp as MyExp
+
+
+class Exp(MyExp):
+    def __init__(self):
+        super(Exp, self).__init__()
+        self.depth = 0.67
+        self.width = 0.75
+        self.exp_name = os.path.split(os.path.realpath(__file__))[1].split(".")[0]
+
+        # Define yourself dataset path
+        self.data_dir = "~/dmeo_obj_detection"
+        self.train_ann = "instances_train2017.json"
+        self.val_ann = "instances_test2017.json"
+        self.input_size = (640,640)
+        self.num_classes = 1
+        self.test_size = (640,640) 
+        self.max_epoch = 100
